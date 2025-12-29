cfea6a982d0d0e2001e644c3e29586a1710db27c
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2025-12-29 09:40:24 +0200

    add AddNewTaskPage and AddNewTaskPageBody widgets for task creation

---
+++ lib/features/home/presentation/pages/add_new_task_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+import 'package:todo_app/features/home/presentation/widgets/add_new_task_page_body.dart';
+
+class AddNewTaskPage extends StatelessWidget {
+  const AddNewTaskPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const AddNewTaskPageBody();
+  }
+}
