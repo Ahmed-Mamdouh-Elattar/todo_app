fecf003b0d372c38043ea688ec64fa8820b2807a
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2025-12-29 12:27:03 +0200

    refactor AddNewTaskPage to include AppBar and navigation functionality

--- lib/features/home/presentation/pages/add_new_task_page.dart
+++ lib/features/home/presentation/pages/add_new_task_page.dart
@@ -1,4 +1,6 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:todo_app/core/config/app_color.dart';
+import 'package:todo_app/core/utils/app_navigation.dart';
 import 'package:todo_app/features/home/presentation/widgets/add_new_task_page_body.dart';
 
 class AddNewTaskPage extends StatelessWidget {
@@ -6,6 +8,22 @@ class AddNewTaskPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const AddNewTaskPageBody();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () {
+            AppNavigation.pop(context);
+          },
+        ),
+        title: const Text(
+          "Add New Task",
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        backgroundColor: AppColor.primary,
+      ),
+      body: const AddNewTaskPageBody(),
+    );
   }
 }
