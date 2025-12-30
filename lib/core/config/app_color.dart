6c333a1bd091fa388182f652bdcb4778c4aae2f8
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2025-12-30 09:26:24 +0200

    add borderColor constant to AppColor class

--- lib/core/config/app_color.dart
+++ lib/core/config/app_color.dart
@@ -3,4 +3,5 @@ import 'package:flutter/widgets.dart';
 class AppColor {
   static const Color primary = Color(0xff4C27A1);
   static const Color secondary = Color(0xffEADEFF);
+  static const Color borderColor = Color(0xff8A71C4);
 }
