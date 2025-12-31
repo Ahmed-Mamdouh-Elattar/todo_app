57761b50eab7c4c3a9f51d9329be2406712b6d12
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2025-12-31 15:44:49 +0200

    apply adding new task and show modal bottom sheet when success and pop

--- lib/features/home/presentation/widgets/add_new_task_page_body.dart
+++ lib/features/home/presentation/widgets/add_new_task_page_body.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todo_app/core/config/app_color.dart';
+import 'package:todo_app/core/config/app_text_styles.dart';
 import 'package:todo_app/core/enums/todo_category.dart';
 import 'package:todo_app/core/helper/constants.dart';
+import 'package:todo_app/core/utils/app_navigation.dart';
 import 'package:todo_app/core/widgets/custom_button.dart';
+import 'package:todo_app/features/home/domain/entities/task_entity.dart';
+import 'package:todo_app/features/home/presentation/providers/database_operation_provider/database_operation_provider.dart';
+import 'package:todo_app/features/home/presentation/providers/database_operation_provider/database_operation_state.dart';
 import 'package:todo_app/features/home/presentation/widgets/date_and_time_picker.dart';
 import 'package:todo_app/features/home/presentation/widgets/notes_field.dart';
 import 'package:todo_app/features/home/presentation/widgets/task_category.dart';
@@ -66,10 +72,66 @@ class AddNewTaskPageBody extends HookWidget {
               outlineInputBorder: outlineInputBorder,
             ),
             const SizedBox(height: 40),
-            CustomButton(
-              text: "Save",
-              onPressed: () {
-                if (formKey.currentState!.validate()) {}
+            Consumer(
+              builder: (context, ref, child) {
+                ref.listen(databaseOperationProvider, (previous, next) {
+                  next.whenOrNull(
+                    success: (task) {
+                      AppNavigation.pop(context);
+                      showModalBottomSheet(
+                        context: context,
+                        backgroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        builder: (context) {
+                          return SizedBox(
+                            width: double.infinity,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                const SizedBox(height: 20),
+                                Text(
+                                  task!.title,
+                                  style: AppTextStyles.semiBold20,
+                                ),
+                                const SizedBox(height: 10),
+                                Text(
+                                  "Task added successfully✅",
+                                  style: AppTextStyles.medium18,
+                                ),
+                                const SizedBox(height: 10),
+                                Text(
+                                  "Do your best to finish the task🦾",
+                                  style: AppTextStyles.medium18,
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                      );
+                    },
+                  );
+                });
+                return CustomButton(
+                  text: "Save",
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {
+                      final databaseOperation = ref.read(
+                        databaseOperationProvider.notifier,
+                      );
+                      final task = TaskEntity(
+                        title: titleController.text,
+                        category: categoryController.value,
+                        date: dateController.text,
+                        time: timeController.text,
+                        notes: notesController.text,
+                        isCompleted: false,
+                      );
+                      databaseOperation.addTask(task);
+                    }
+                  },
+                );
               },
             ),
             const SizedBox(height: 30),
