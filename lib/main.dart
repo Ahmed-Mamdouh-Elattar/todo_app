ac45e6ec640f33eae954d97e0a01902ad297392a
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2025-12-31 06:01:04 +0200

    refactor main function to initialize ObjectBox and set up provider scope

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:todo_app/core/services/object_box/object_box.dart';
+import 'package:todo_app/core/services/object_box/object_box_provider.dart';
 import 'package:todo_app/features/home/presentation/pages/home_page.dart';
 
-void main() {
-  runApp(const ProviderScope(child: TodoApp()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final objectBox = ObjectBox.instance;
+  await objectBox.init();
+  runApp(
+    ProviderScope(
+      overrides: [objectBoxProvider.overrideWithValue(objectBox)],
+      child: const TodoApp(),
+    ),
+  );
 }
 
 class TodoApp extends StatelessWidget {
