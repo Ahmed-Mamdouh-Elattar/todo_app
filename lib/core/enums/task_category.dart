50a943e06d0b65f87a2c9b1b33fe5c511478301c
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2025-12-31 04:42:32 +0200

    add TaskCategory enum for task categorization with associated colors and icons

---
+++ lib/core/enums/task_category.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+enum TaskCategory {
+  work(color: Colors.blue, icon: Icons.work),
+  personal(color: Colors.green, icon: Icons.person),
+  home(color: Colors.orange, icon: Icons.home),
+  shopping(color: Colors.purple, icon: Icons.shopping_cart),
+  health(color: Colors.red, icon: Icons.local_hospital),
+  finance(color: Colors.teal, icon: Icons.account_balance_wallet),
+  education(color: Colors.indigo, icon: Icons.school),
+  travel(color: Colors.pink, icon: Icons.flight_takeoff),
+  social(color: Colors.cyan, icon: Icons.people),
+  others(color: Colors.grey, icon: Icons.calendar_month);
+
+  final Color color;
+  final IconData icon;
+  const TaskCategory({required this.color, required this.icon});
+}
